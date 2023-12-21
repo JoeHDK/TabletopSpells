1a4fcd32875b3f0d2fc5edb13fd6b212228a2391
JoeHDK <joe_hansen@live.dk>
2023-12-21 18:58:00 +0100

    Add project files.

---
+++ TabletopSpells/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TabletopSpells
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TabletopSpells/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TabletopSpells
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
